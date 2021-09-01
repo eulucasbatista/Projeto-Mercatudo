b2552e884356df38940cce1642b8db18ef675a68
eulucasbatista <lucas.batista20@gmail.com>
2021-08-31 23:55:20 -0300

    Mercatudo

---
+++ mercatudo/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Mercatudo</title>
+
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	   
+	    <link href="css/css.css" rel="stylesheet">
+	    <link href="css/e-modal.css" rel="stylesheet">
+	 	<link href="css/font-awesome.min.css" rel="stylesheet">
+	 	<link href="css/font-awesome.css" rel="stylesheet">
+		<link href="css/font.css" rel="stylesheet">
+		
+		<link rel="shortcut icon" href="img/logo_pequeno.ico" type="image/x-icon" />
+	 
+	    <script src="js/jquery_v1.11.3.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	    <script type="text/javascript" src="js/bootstrap-3.3.4-js-bootstrap.min.js"></script>
+	    <script type="text/javascript" src="js/jquery-2.0.3-jquery.min.js"></script>
+
+
+	</head>
+
+	<body>
+		<div class="container-fluid principal">	
+			<?php include 'frontend/menu.php';?>
+			<?php include 'frontend/sobre.html';?>
+			<?php include 'frontend/doacao.html';?>
+			<?php include 'frontend/produtos.html';?>
+			<?php include 'frontend/rodape.html';?>
+			
+
+		
+		</div> <!-- final div container-fluid-->
+	</body>
+</html>
