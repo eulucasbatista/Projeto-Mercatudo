fdb87e886818a65debc6d8931c6425dcddcbbbaf
Engage-Lucas-Batista <65094987+eulucasbatista@users.noreply.github.com>
2022-02-15 16:17:39 -0300

    Projeto Mercatudo
    Projeto feito no 4º Semestre do Curso de Informática para Internet, oferecido pelo IFSP-GRU

---
+++ frontend/menu.php
@@ -0,0 +1,92 @@
+
+    <div>
+      <div class="container">
+        <div class="hidden-xs navbar-header">
+          <button type="button" class="navbar-toggle collapse navbar-collapse" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="hidden-xs navbar-brand"><img height="60" src="img/logo.png"></a>
+        </div>
+        <div class="collapse hidden-xs navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#sobre">Sobre</a>
+            </li>
+            <li class="hidden-xs">
+              <a href="#doacao">Doação</a>
+            </li>
+            <li class="hidden-xs">
+              <a href="#produtos">Produtos</a>
+            </li>
+            <li class="hidden-xs">
+              <a href="#contato">Contato</a>
+            </li>
+            <li>
+              <a href="#login" data-toggle="modal">
+
+					<i class="fa fa-fw fa-user">
+
+					</i>Login</a>
+            </li>
+            <li>
+              <a href="#cadastro" data-toggle="modal">
+
+					<i class="fa fa-fw fa-user-plus">
+
+					</i>Cadastrar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <img height="60" src="img/logo.png" class="hidden-lg hidden-md hidden-sm img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden-lg hidden-md hidden-sm section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="hidden-lg hidden-md hidden-sm nav nav-pills menu-celular">
+              <li class="hidden-lg hidden-md hidden-sm">
+                <a href="#sobre">Sobre</a>
+              </li>
+              <li class="hidden-lg hidden-md hidden-sm">
+                <a href="#doacao">Doação</a>
+              </li>
+              <li class="hidden-lg hidden-md hidden-sm">
+                <a href="#produtos">Produtos</a>
+              </li>
+              <li class="hidden-lg hidden-md hidden-sm">
+                <a href="#contato">Contato</a>
+              </li>
+              <li class="hidden-lg hidden-md hidden-sm menu-celular">
+                <a href="#login" data-toggle="modal">
+
+					<i class="fa fa-fw fa-user">
+
+					</i>Login</a>
+              </li>
+              <li class="hidden-lg hidden-md hidden-sm">
+                <a href="#cadastro" data-toggle="modal">
+
+					<i class="fa fa-fw fa-user-plus">
+
+					</i>Cadastro</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include 'frontend/modal.html';?>
+  
+
